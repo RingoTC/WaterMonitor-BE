d5a3288d1cb6e8dc5851d0f9bab47a1c441c74cb
ringotc <ringotc@outlook.com>
2023-12-11 16:01:55 -0800

    fix signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,21 +5,60 @@ const User = require('../models/user');
 const passport = require('passport');
 
 exports.register = (req, res) => {
-    const { username, password } = req.body;
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
-        const newUser = new User({
-            username: username,
-            password: hashedPassword,
-        });
-        newUser.save()
-            .then((user) => {
-                res.send('User registered successfully');
-            })
-            .catch((err) => {
-                res.status(500).send('Error saving user');
-            });
+    const {
+        username,
+        password,
+        firstName,
+        lastName,
+        email,
+        role,
+        company,
+        cellphone,
+        city,
+        country,
+        like
+    } = req.body;
 
-    });
+    // Check if the username already exists in the database
+    User.findOne({ username: username })
+        .then(existingUser => {
+            if (existingUser) {
+                // If username already exists, send an error response
+                return res.status(400).send('Username already exists');
+            }
+
+            // If username doesn't exist, hash the password and save the new user
+            bcrypt.hash(password, 10, (err, hashedPassword) => {
+                if (err) {
+                    return res.status(500).send('Error hashing password');
+                }
+
+                const newUser = new User({
+                    username: username,
+                    password: hashedPassword,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    role: role,
+                    company: company,
+                    cellphone: cellphone,
+                    city: city,
+                    country: country,
+                    like: like
+                });
+
+                newUser.save()
+                    .then((user) => {
+                        res.send('User registered successfully');
+                    })
+                    .catch((err) => {
+                        res.status(500).send('Error saving user');
+                    });
+            });
+        })
+        .catch(err => {
+            res.status(500).send('Error checking for existing user');
+        });
 };
 
 exports.login = (req, res, next) => {
